2cd9d9daaf1045a37cee34a9c86f3a6841130bcf
Roman Danchevskyi <mrdak333@gmail.com>
2020-01-21 20:25:14 +0200

    add-slider

---
+++ src/js/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.main-slider').slick(  {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    accessibility: true,
+    arrows: true,
+
+  });
+});
+
